0afbb0225cc494b8e2ca9522a862c3bad08bdc85
Puneeth S <117469967+puneeths11@users.noreply.github.com>
2024-04-27 13:53:03 +0530

    Update SearchBox.jsx

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -7,7 +7,7 @@ export default function SearchBox({ updateInfo }) {
   let [city, setCity] = useState("");
   let [error, setError] = useState(false);
   const API_URL = "https://api.openweathermap.org/data/2.5/weather";
-  const API_KEY = "9c0ffc437eec9c44c4f8c57dbb288616";
+  const API_KEY = "";
 
   let getWeatherInfo = async () => {
     try {
